b77e350991c7fe4da308ddc3a56fd7c189dda248
NordstedtSweden <martin.nordstedt@hotmail.com>
2026-04-15 14:31:39 +0200

    Initialize Express server with Socket.IO support

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+const spel = {};
+
+io.on('connection', socket => {
+
+  socket.on('gå-med', ({ kod, roll }) => {
+    socket.join(kod);
+    socket.data.kod = kod;
+    socket.data.roll = roll;
+    if (!spel[kod]) spel[kod] = {};
+    console.log(`${roll} anslöt till spel ${kod}`);
+    io.to(kod).emit('ansluten', { roll, kod });
+  });
+
+  socket.on('state', data => {
+    socket.to(data.kod).emit('state', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Frånkopplad:', socket.data.roll);
+  });
+
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('Server igång på port', PORT);
+});
