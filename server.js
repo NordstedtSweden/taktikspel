ae8f2057ebddf9af6425d03fe69628bfcccc2383
NordstedtSweden <martin.nordstedt@hotmail.com>
2026-04-16 17:28:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: '*' }
+});
+
+app.use('/kartor', express.static(path.join(__dirname, 'public/kartor')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Taktikspel server körs!');
 });
 
 const spel = {};
